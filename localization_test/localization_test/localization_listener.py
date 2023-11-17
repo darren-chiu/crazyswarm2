3a7f5e0d09aca264d96fb865e7461386d4caa7b9
Darren Chiu <darrenchiu321@gmail.com>
2023-11-16 18:06:30 -0800

    Added filter

--- localization_test/localization_test/localization_listener.py
+++ localization_test/localization_test/localization_listener.py
@@ -10,11 +10,13 @@ from crazyflie_py import Crazyswarm
 from crazyflie_py.uav_trajectory import Trajectory
 from crazyflie_interfaces.msg import LogDataGeneric  
 from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import TransformStamped
 from tf2_msgs.msg import TFMessage
 
 #ROS2 Node Imports
 from rclpy.node import Node
 import rclpy
+from message_filters import TimeSynchronizer, Subscriber
 
 #Transform Imports
 from tf2_ros import TransformException
@@ -27,27 +29,22 @@ class LocalizationSubscriber(Node):
     def __init__(self):
         super().__init__('localization_subscriber')
 
-        self.onboard_subscription = self.create_subscription(
-            PoseStamped,                                               
-            '/cf1/pose',
-            self.onboard_callback,
-            10)
-        self.onboard_subscription
+        self.onboard_subscription = Subscriber(self, PoseStamped, "/cf1/pose")
+        self.vicon_subscription = Subscriber(self, PoseStamped, "/cf2/pose")
 
-        self.vicon_subscription = self.create_subscription(
-            PoseStamped,                                               
-            '/cf1/pose',
-            self.vicon_callback,
-            10)
-        self.vicon_subscription
+        self.tss_localization = TimeSynchronizer([self.onboard_subscription, self.vicon_subscription], queue_size=0.01)
+        self.tss_localization.registerCallback(self.diff_callback)
+    # def onboard_callback(self, frame):
+    #         # print(stamped.pose.position.x)
+    #         self.get_logger().info('Onboard Estimation: (%s)' % frame.pose.position.x)  # CHANGE
 
-    def onboard_callback(self, frame):
-            # print(stamped.pose.position.x)
-            self.get_logger().info('Onboard Estimation: (%s)' % frame.pose.position.x)  # CHANGE
+    # def vicon_callback(self, frame):
+    #         # print(stamped.pose.position.x)
+    #         self.get_logger().info('Vicon Estimation: (%s)' % frame.pose.position.x)  # CHANGE
 
-    def vicon_callback(self, frame):
-            # print(stamped.pose.position.x)
-            self.get_logger().info('Vicon Estimation: (%s)' % frame.pose.position.x)  # CHANGE
+    def diff_callback(onboard, vicon):
+        self.get_logger().info('Onboard Estimation: (%s)' % onboard.pose.position.x)
+        self.get_logger().info('Vicon Estimation: (%s)' % vicon.pose.position.x)
 
 def main(args=None):
     rclpy.init(args=args)
